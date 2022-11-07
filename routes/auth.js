5162643518d5af7cdd9e0046ceb84a3ffbaaf55f
Christopher Lim <cli3338198@gmail.com>
2022-11-07 14:54:11 -0800

    finished auth

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,38 @@
 
 const Router = require("express").Router;
 const router = new Router();
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config");
+const { UnauthorizedError, BadRequestError } = require("../expressError");
+const User = require("../models/user");
 
 /** POST /login: {username, password} => {token} */
+router.post("/login", async function (req, res) {
+  if (req.body === undefined) throw new BadRequestError();
 
+  const { username, password } = req.body;
+
+  if ((await User.authenticate(username, password)) === true) {
+    await User.updateLoginTimestamp(username);
+    const token = jwt.sign({ username }, SECRET_KEY);
+    return res.json({ token });
+  }
+
+  throw new UnauthorizedError("You must be logged in to do that.");
+});
 
 /** POST /register: registers, logs in, and returns token.
  *
  * {username, password, first_name, last_name, phone} => {token}.
  */
+router.post("/register", async function (req, res) {
+  if (req.body === undefined) throw new BadRequestError();
+  try {
+    const user = await User.register(req.body);
+    return user && jwt.sign({ username: user.username }, SECRET_KEY);
+  } catch (err) {
+    throw err;
+  }
+});
 
 module.exports = router;
