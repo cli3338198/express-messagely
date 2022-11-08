4bafb4167d2c8d16898955bff2ed9e6d8f77bc04
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-11-08 11:56:20 -0800

    Refactored tests

--- routes/users.test.js
+++ routes/users.test.js
@@ -14,9 +14,8 @@ describe("Users Routes Test", function () {
   let u2;
   let m1;
   let m2;
-  let _token;
-  let _badToken;
-  //TODO: Rename tokens
+  let u1Token;
+  let u2Token;
 
   beforeEach(async function () {
     await db.query("DELETE FROM messages");
@@ -50,17 +49,20 @@ describe("Users Routes Test", function () {
       body: "Yo",
     });
 
-    _token = jwt.sign({ username: u1.username }, SECRET_KEY);
-    _badToken = jwt.sign({ username: u2.username }, SECRET_KEY);
+    u1Token = jwt.sign({ username: u1.username }, SECRET_KEY);
+    u2Token = jwt.sign({ username: u2.username }, SECRET_KEY);
   });
 
   describe("GET /users/", function () {
     test("Get all users", async function () {
-      const resp = await request(app).get("/users").query({ _token });
+      const resp = await request(app).get("/users").query({ _token: u1Token });
 
       expect(resp.statusCode).toEqual(200);
       expect(resp.body.users.length).toEqual(2);
-      // TODO: expect exact data from users
+      expect(resp.body.users).toEqual(
+        [{username: "test1", first_name: "Test1", last_name: "Testy1"},
+         {username: "test2", first_name: "Test2", last_name: "Testy2"}]
+      );
     });
 
     test("Won't get all users if not logged in", async function () {
@@ -74,7 +76,7 @@ describe("Users Routes Test", function () {
     test("Get user detail", async function () {
       const resp = await request(app)
         .get(`/users/${u1.username}`)
-        .query({ _token });
+        .query({ _token: u1Token });
 
       expect(resp.statusCode).toEqual(200);
       expect(resp.body.user).toEqual({
@@ -85,7 +87,6 @@ describe("Users Routes Test", function () {
         join_at: expect.any(String),
         last_login_at: expect.any(String),
       });
-      // TODO: What happens if user does not exist
     });
 
     test("Won't get user detail if not logged in", async function () {
@@ -93,23 +94,41 @@ describe("Users Routes Test", function () {
 
       expect(resp.statusCode).toEqual(401);
     });
+
+    test("Won't get user detail if user does not exist", async function () {
+      const resp = await request(app)
+        .get("/users/FAKEUSER")
+        .query({ _token: u1Token });
+
+      expect(resp.statusCode).toEqual(401);
+    })
   });
 
   describe("GET /users/:username/to", function () {
     test("Get messages to a user", async function () {
       const resp = await request(app)
         .get(`/users/${u1.username}/to`)
-        .send({ _token });
+        .send({ _token: u1Token });
 
       expect(resp.statusCode).toEqual(200);
       expect(resp.body.messages.length).toEqual(1);
-      expect(resp.body.messages[0].body).toEqual("Yo"); // TODO: Be more exact
+      expect(resp.body.messages).toEqual([
+        {id: expect.any(Number),
+         body: "Yo",
+         sent_at: expect.any(String),
+         read_at: null,
+         from_user: {
+          username: u2.username,
+          first_name: "Test2",
+          last_name: "Testy2",
+          phone: "+14155550001"
+        }}]);
     });
 
     test(`Won't get messages to user not the recipient`, async function () {
       const resp = await request(app)
         .get(`/users/${u1.username}/to`)
-        .send({ _token: _badToken }); // Wrong auth
+        .send({ _token: u2Token }); // Wrong auth
 
       expect(resp.statusCode).toEqual(401);
     });
@@ -120,24 +139,40 @@ describe("Users Routes Test", function () {
       expect(resp.statusCode).toEqual(401);
     });
 
-    // TODO: User does not exist
+    test("Won't get messages if user does not exist", async function () {
+      const resp = await request(app)
+        .get("/users/FAKEUSER/to")
+        .query({ _token: u1Token });
+
+      expect(resp.statusCode).toEqual(401);
+    })
   });
 
   describe("GET /users/:username/from", function () {
     test("Get messages from a user", async function () {
       const resp = await request(app)
         .get(`/users/${u1.username}/from`)
-        .send({ _token });
+        .send({ _token: u1Token });
 
       expect(resp.statusCode).toEqual(200);
       expect(resp.body.messages.length).toEqual(1);
-      expect(resp.body.messages[0].body).toEqual("Hello"); // TODO: Be more exact
+      expect(resp.body.messages).toEqual([
+        {id: expect.any(Number),
+         body: "Hello",
+         sent_at: expect.any(String),
+         read_at: null,
+         to_user: {
+          username: u2.username,
+          first_name: "Test2",
+          last_name: "Testy2",
+          phone: "+14155550001"
+        }}]);
     })
 
     test("Won't get messages from user if not correct user", async function () {
       const resp = await request(app)
         .get(`/users/${u1.username}/from`)
-        .send({ _token: _badToken }); // Wrong auth
+        .send({ _token: u2Token }); // Wrong auth
 
       expect(resp.statusCode).toEqual(401);
     });
@@ -148,7 +183,13 @@ describe("Users Routes Test", function () {
       expect(resp.statusCode).toEqual(401);
     })
 
-    // TODO: User does not exist
+    test("Won't get user detail if user does not exist", async function () {
+      const resp = await request(app)
+        .get("/users/FAKEUSER/from")
+        .query({ _token: u1Token });
+
+      expect(resp.statusCode).toEqual(401);
+    })
   });
 });
 
